7a7d2eb661eaa9f140423f38d3341445cdc23805
Mateusz Oginski <235936@edu.p.lodz.pl>
2023-05-28 15:28:59 +0200

    feat: display delegated tasks

--- src/components/my_task/MyTask.jsx
+++ src/components/my_task/MyTask.jsx
@@ -1,22 +1,14 @@
 import React, { useState } from "react";
 
-const MyTask = () => {
+const MyTask = ({ task: task }) => {
   return (
-    <>
-      <h2 className="text-2xl font-bold mb-4 ml-4 text-white">
-        Delegated tasks
-      </h2>
-      <div className="tasks bg-gray-100 p-4 ml-8 rounded-lg shadow-md my-4 w-1/2">
-        {/* Your code to display delegated tasks */}
-      </div>
-      <h2 className="text-2xl font-bold mb-4 ml-4 text-white">
-        Tasks to be completed
-      </h2>
-      <div className="tasks bg-gray-100 p-4 ml-8 rounded-lg shadow-md my-4 w-1/2">
-        {/* Your code to display tasks to be completed */}
-      </div>
-    </>
-  );
+    <div className="task-details">
+      <h4>{task.content}</h4>
+      <p><strong>Cost: </strong>{task.koszt}</p>
+      <p>{task.data}</p>
+      <p>Languages: {task.languages.join(', ')}</p>
+    </div>
+  )
 };
 
 export default MyTask;
--- src/components/my_task/My_Tasks.jsx
+++ src/components/my_task/My_Tasks.jsx
@@ -2,45 +2,56 @@ import React from 'react';
 import { useEffect, useState } from "react"
 
 import MyTask from './MyTask';
-// import '../tasks/tasks.css'
 
 const MyTasks = () => {
+  const [tasks, setTasks] = useState(null);
+  const user = JSON.parse(localStorage.getItem('user'));
+  const email = user.email;
 
-    const [tasks, setTasks] = useState(null)
-    const user = JSON.parse(localStorage.getItem('user'));
-    const email = user.email;
+  useEffect(() => {
+    const fetchTasks = async () => {
+      const response = await fetch('/api/tasks/my_tasks', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email }),
+      });
+      // const response = await fetch('/api/tasks')
+      const json = await response.json();
 
-    useEffect(() => {
-      const fetchTasks = async () => {
-        const response = await fetch('/api/tasks/my_tasks', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({ email }),
-        });
-        const json = await response.json()
-        
-        if (response.ok) {
-          setTasks(json)
-          console.log(json)
-          console.log(tasks)
-        }
+      if (response.ok) {
+        setTasks(json);
+        console.log(json); // Poprawnie ustawione wartości tasks
       }
+    }
+
+    fetchTasks();
+  }, [])
   
-      fetchTasks()
-    }, [])
-  
-    return (
-      <div className="home">
-        <div className="tasks">
-          {tasks && tasks.map(task => (
-            <MyTask task={task} key={task._id} />
-          ))}
+  return (
+    <>
+      <h2 className="text-2xl font-bold mb-4 ml-4 text-white">
+        Delegated tasks
+      </h2>
+      <div className="tasks bg-gray-100 p-4 ml-8 rounded-lg shadow-md my-4 w-1/2">
+        {/* Your code to display delegated tasks */}
+        <div className="home">
+          <div className="tasks">
+            {tasks && tasks.map(task => (
+              <MyTask task={task} key={task._id} />
+            ))}
+          </div>
         </div>
       </div>
-    )
-  }
-
+      <h2 className="text-2xl font-bold mb-4 ml-4 text-white">
+        Tasks to be completed
+      </h2>
+      <div className="tasks bg-gray-100 p-4 ml-8 rounded-lg shadow-md my-4 w-1/2">
+        {/* Your code to display tasks to be completed */}
+      </div>
+    </>
+  );
+}
 
 export default MyTasks;
